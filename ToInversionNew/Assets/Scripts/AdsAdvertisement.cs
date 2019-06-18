e8a1ae1a2d413dcf6182d1e4c256e96f94ac3cc9
Dinar Fakhriev <dinar.fahriev@yandex.ru>
2019-06-18 19:26:00 +0300

    18.06 TextMesh Files Update

---
+++ ToInversionNew/Assets/Scripts/AdsAdvertisement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsAdvertisement : MonoBehaviour
+{
+    public static AdsAdvertisement instance = null; // Экземпляр объекта
+
+    private string store_id = "3131989";
+    private string banner = "banner";
+
+    // Метод, выполняемый при старте игры
+    void Start()
+    {
+        // Теперь, проверяем существование экземпляра
+        if (instance == null)
+        { // Экземпляр менеджера был найден
+            instance = this; // Задаем ссылку на экземпляр объекта
+        }
+        else if (instance == this)
+        { // Экземпляр объекта уже существует на сцене
+            Destroy(gameObject); // Удаляем объект
+        }
+
+        // Теперь нам нужно указать, чтобы объект не уничтожался
+        // при переходе на другую сцену игры
+        DontDestroyOnLoad(gameObject);
+
+        InitializeManager();
+
+    }
+
+    // Метод инициализации менеджера
+    private void InitializeManager()
+    {
+        /* TODO: Здесь мы будем проводить инициализацию */
+        Advertisement.Initialize(store_id, true);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady(banner))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+        Advertisement.Banner.Show(banner);
+    }
+}
+
---
+++ ToInversionNew/Assets/Scripts/AdsMonetization.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Monetization;
+
+public class AdsMonetization : MonoBehaviour
+{
+    public static AdsMonetization instance = null; // Экземпляр объекта
+
+    private string video = "video";
+    private string rewardedVideo = "rewardedVideo";
+
+    // Метод, выполняемый при старте игры
+    void Start()
+    {
+        // Теперь, проверяем существование экземпляра
+        if (instance == null)
+        { // Экземпляр менеджера был найден
+            instance = this; // Задаем ссылку на экземпляр объекта
+        }
+        else if (instance == this)
+        { // Экземпляр объекта уже существует на сцене
+            Destroy(gameObject); // Удаляем объект
+        }
+
+        // Теперь нам нужно указать, чтобы объект не уничтожался
+        // при переходе на другую сцену игры
+        DontDestroyOnLoad(gameObject);
+
+        InitializeManager();
+
+    }
+
+    // Метод инициализации менеджера
+    private void InitializeManager()
+    {
+        /* TODO: Здесь мы будем проводить инициализацию */
+
+    }
+
+
+}
